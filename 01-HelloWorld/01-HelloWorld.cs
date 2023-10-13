886c6045e0a5279832bb2ba7875421c2b2e406c7
the-destroyer1 <147569935+the-destroyer1@users.noreply.github.com>
2023-10-13 12:32:24 +1100

    it worked
    it worked

--- 01-HelloWorld/01-HelloWorld.cs
+++ 01-HelloWorld/01-HelloWorld.cs
@@ -13,7 +13,7 @@ Main Tasks:
 
 Questions:
 1. What is the difference between input and output?
-input is what you put in and output is what the code shows
+input is what you put in and output is what the code sh`ows
 Extension Tasks:
 1. Output another message / pause after the ReadLine.
 2. Research online how to change the colour and/or make the console beep.
@@ -33,11 +33,14 @@ namespace ProgrammingExercisesIST
             // Your code goes below here
             Console.WriteLine("hello World");
             Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Red;
             Console.WriteLine("goodbye");
             Console.ReadLine();
             Console.Beep(1000, 5000);
             Console.ReadLine();
-            ConsoleColor foreground = ConsoleColor.Red;
+            
+           
         }
     }
 }
@@ -47,7 +50,29 @@ namespace ProgrammingExercisesIST
 FEEDBACK AREA: Paste your AI feedback here before submission:
 ===============================================================================
 
+Feedback on your C# Programming Task:
 
+Code:
+
+Your code for the main tasks is correct. You have successfully used Console.WriteLine(); to output 'Hello World' and Console.ReadLine(); to make the program wait for user input.
+
+For the extension tasks, you have correctly implemented the Console.Beep(); method to make the console beep. Good job on that!
+
+However, you have declared the ConsoleColor foreground variable but didn't use it to change the console color. You need to use Console.ForegroundColor = foreground; to actually change the console color.
+
+Code Commenting:
+
+Your code comments are clear and concise, providing a good understanding of what each part of your code is doing.
+Answers to Questions:
+
+Your answer to the question is correct. Input is the data that you provide to the program, and output is what the program returns after processing the input.
+Extension Tasks:
+
+You have successfully completed the first extension task by outputting another message and pausing after the ReadLine.
+
+For the second extension task, you have correctly used the Console.Beep(); method to make the console beep, but you need to use the Console.ForegroundColor property to change the console color.
+
+Overall, you are doing a great job! Keep practicing and experimenting with different features of C#.
 
 ===============================================================================
 */
--- 02-Variables/02-Variables.cs
+++ 02-Variables/02-Variables.cs
@@ -13,8 +13,8 @@ Main Tasks:
 3. Make the program wait for input.
 
 Questions:
-1. What is a variable?
-2. What is a string?
+1. What is a variable? a variable is something that can change
+2. What is a string? a string is when you set aside memory and give it a value
 
 Extension Tasks:
 1. Try creating two string variables and using them in the same message.
@@ -34,7 +34,9 @@ namespace ProgrammingExercisesIST
         static void Main(string[] args)
         {
             // Your code goes below here
-            
+            string name = "linc";
+            Console.WriteLine("Hello + name");
+            Console.ReadLine();
 
         }
     }
@@ -44,7 +46,24 @@ namespace ProgrammingExercisesIST
 ===============================================================================
 FEEDBACK AREA: Paste your AI feedback here before submission:
 ===============================================================================
+Feedback on your C# Programming Task:
 
+Code:
+
+Your string variable declaration is correct. Good job!
+However, when you're outputting the variable in Console.WriteLine(), you need to remove the quotes around name for it to be recognized as a variable. The correct way would be Console.WriteLine("Hello " + name);.
+Code Commenting:
+
+Your code is well-commented, and it's easy to understand what the task is.
+Answers to Questions:
+
+Your explanation of a variable is correct but could be more detailed. A variable is a named space in the memory where a programmer can store data and later retrieve the data using the variable name.
+Your answer to what a string is partially correct. While it is true that a string involves setting aside memory, a more accurate definition would be: A string is a sequence of characters.
+Extension Tasks:
+
+It seems you haven't attempted the extension tasks. When you do, remember to create two string variables and use them in the same message for the first task.
+For the second task, research how to change a string to uppercase or lowercase in C#. The ToUpper() and ToLower() methods should be helpful.
+Overall, you're on the right track. Keep practicing and don't forget to attempt the extension tasks for more practice. Keep it up!
 
 
 ===============================================================================
